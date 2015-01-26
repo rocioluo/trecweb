8163a8c3934f45a429d4e2d896548ad5ee881d3a
rocioluo <rocioluo@gmail.com>
2015-01-26 00:48:21 -0500

    trecweb

--- src/edu/pitt/sis/infsci2140/index/TrectextCollection.java
+++ src/edu/pitt/sis/infsci2140/index/TrectextCollection.java
@@ -10,6 +10,8 @@ import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 
 /**
  * This is for INFSCI 2140 in 2015
@@ -18,83 +20,90 @@ import java.util.logging.Logger;
 public class TrectextCollection implements DocumentCollection {
 	
 	// YOU SHOULD IMPLEMENT THIS METHOD
-	  
-
-    void getdata(String args[]) {
-	      
-	        //reading file line by line in Java using BufferedReader       
-	        FileInputStream fis = null;
-	        BufferedReader reader = null;
-	      
-	        try {
-	            fis = new FileInputStream("D://pitt//data retrieval//docset//docset.trectext");
-	            reader = new BufferedReader(new InputStreamReader(fis));    
-	            
-	            String line = reader.readLine();    
-	            String tempkey;
-	            String key = null;
-	            String text = null;
-	            Map<String,Object> map= new HashMap<String,Object>();
-	            
-	            while (line!= null){
-	            	
-                line=reader.readLine();
-                
-	                while (line.equals("<DOC>")){	                	
-	            	line=reader.readLine();
-	            	tempkey=line;
-                    key= tempkey.substring(7);                   
-                break;   
-                }
-	                
-	            while (line!="<TEXT>"){
-	            	line=reader.readLine();
-	            }
-	            
-                while (line!="<TEXT>"){
-                    	line=reader.readLine();
-                    	text=line;
-                    	text+=text;
-                }
-                         map.put(key,text);
-                         line=reader.readLine();
-                         line=reader.readLine();
-                }
-	            
-	        }
-
-                    
-	            
-	          
-	        catch (FileNotFoundException ex) {
-	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	        } catch (IOException ex) {
-	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	          
-	        } 
-	        finally {
-	            try {
-	                reader.close();
-	                fis.close();
-	            } catch (IOException ex) {
-	                Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	            }
-	        
-	        }
-    }
-    
+	 
+   
 	public TrectextCollection( FileInputStream instream ) throws IOException {
 		// This constructor should take an inputstream of the collection file as the input parameter.
-		
+		  //reading file line by line in Java using BufferedReader       
+        FileInputStream fis = null;
+        BufferedReader reader = null;
+        Map<String,Object> map= new HashMap<String,Object>();
+      
+        try {
+            fis = new FileInputStream("D://pitt//data retrieval//docset//docset.trectext");
+            reader = new BufferedReader(new InputStreamReader(fis));    
+            
+            String line = reader.readLine();    
+            String tempkey;
+            String key = null;
+            String text = null;
+            
+            
+            while (line!= null){
+            	
+            line=reader.readLine();
+            
+                while (line.equals("<DOC>")){	                	
+            	line=reader.readLine();
+            	tempkey=line;
+                key= tempkey.substring(8,15);                   
+            break;   
+            }
+                
+            while (line!="<TEXT>"){
+            	line=reader.readLine();
+            }
+            
+            while (line!="<TEXT>"){
+                	line=reader.readLine();
+                	text=line;
+                	text+=text;
+            }
+                     map.put(key,text);
+                     line=reader.readLine();
+                     line=reader.readLine();
+            }
+            Iterator iter = (Iterator) map.entrySet().iterator(); 
+           // Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
+    	
+    		
+            
+        }
+
+                
+          
+          
+        catch (FileNotFoundException ex) {
+            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+          
+        } 
+        finally {
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        }
 	}
 	
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public Map<String, Object> nextDocument() throws IOException {
+		
 		// Read the definition of this method from edu.pitt.sis.infsci2140.index.DocumentCollection interface 
 		// and follow the assignment instructions to implement this method.
+		Map map2 = new HashMap(); 
+		if(iter.hasNext()) { 
+ 		    Map.Entry entry = (Map.Entry) iter.next(); 
+ 		    Object key = entry.getKey(); 
+ 		    Object val = entry.getValue(); 
+ 		    //text:stringto char
 		
 		
 		return null;
 	}
 	
+}
 }
--- src/edu/pitt/sis/infsci2140/index/TrecwebCollection.java
+++ src/edu/pitt/sis/infsci2140/index/TrecwebCollection.java
@@ -1,8 +1,18 @@
 package edu.pitt.sis.infsci2140.index;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 /**
  * This is for INFSCI 2140 in 2015
@@ -13,6 +23,70 @@ public class TrecwebCollection implements DocumentCollection {
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public TrecwebCollection( FileInputStream instream ) throws IOException {
 		// This constructor should take an inputstream of the collection file as the input parameter.
+		  FileInputStream fis = null;
+	        BufferedReader reader = null;
+	        Map<String,Object> map= new HashMap<String,Object>();
+	        try {
+	            fis = new FileInputStream("D://pitt//data retrieval//docset//docset.trectweb");
+	            reader = new BufferedReader(new InputStreamReader(fis));    
+	            String line = reader.readLine(); 
+	            line=line.replaceAll("<script[^>]*?>[£Ü£Üs£Ü£ÜS]*?<£Ü£Ü/script>", "");
+	            line=line.replaceAll("<[£Ü£Üs]*?style[^>]*?>[£Ü£Üs£Ü£ÜS]*?<[£Ü£Üs]*?£Ü£Ü/[£Ü£Üs]*?style[£Ü£Üs]*?>", "");
+	            line=line.replaceAll("</?(?!/?p)[^>]*>", "");
+	            String tempkey;
+	            String key = null;
+	            String text = null;
+	            
+	            
+	            while (line!= null){
+	            	
+	            line=reader.readLine();
+	            
+	                while (line.equals("<DOC>")){	                	
+	            	line=reader.readLine();
+	            	tempkey=line;
+	                key= tempkey.substring(8,15);                   
+	            break;   
+	            }
+	                
+	            while (line!="<p>"){
+	            	line=reader.readLine();
+	            }
+	            
+	            while (line!="</p>"){
+	                	line=reader.readLine();
+	                	text=line;
+	                	text+=text;
+	            }
+	                     map.put(key,text);
+	                     line=reader.readLine();
+	                     line=reader.readLine();
+	            }
+	            Iterator iter = (Iterator) map.entrySet().iterator(); 
+	           // Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
+	    	
+	    		
+	            
+	        }
+
+	                
+	          
+	          
+	        catch (FileNotFoundException ex) {
+	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+	        } catch (IOException ex) {
+	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+	          
+	        } 
+	        finally {
+	            try {
+	                reader.close();
+	                fis.close();
+	            } catch (IOException ex) {
+	                Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
+	            }
+	        
+	        }
 	}
 	
 	// YOU SHOULD IMPLEMENT THIS METHOD
