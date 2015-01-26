4d56c700e026bcc0853a735b4328d0f4a738bead
rocioluo <rocioluo@gmail.com>
2015-01-26 13:23:18 -0500

    update

--- src/edu/pitt/sis/infsci2140/analysis/StopwordsRemover.java
+++ src/edu/pitt/sis/infsci2140/analysis/StopwordsRemover.java
@@ -1,6 +1,13 @@
 package edu.pitt.sis.infsci2140.analysis;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+import com.sun.istack.internal.logging.Logger;
 
 /**
  * This is for INFSCI 2140 in 2015
@@ -12,10 +19,44 @@ public class StopwordsRemover {
 	public StopwordsRemover( FileInputStream instream ) {
 		// load and store the stop words from the fileinputstream with appropriate data structure
 		// that you believe is suitable for matching stop words.
-	}
+		FileInputStream fis = null;
+        BufferedReader reader = null;
+        char[] stopword=new char[20000];//Initiate
+      
+        try {
+            fis = new FileInputStream("D://pitt//data retrieval//stop_list.txt");
+            reader = new BufferedReader(new InputStreamReader(fis));        
+            String line = reader.readLine();
+            
+            
+            while(line != null){
+                System.out.println(line);
+                line = reader.readLine();
+            }  
+            for (int i =0;i<line.length();i++)
+            {
+            	stopword[i]=line.charAt(i);
+            }//read into char array 
+          
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StopwordsRemover.class.getName(),null).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(StopwordsRemover.class.getName(),null).log(Level.SEVERE, null, ex);
+          
+        } finally {
+            try {
+                reader.close();
+                fis.close();
+            } catch (IOException ex) {
+                Logger.getLogger(StopwordsRemover.class.getName(), null).log(Level.SEVERE, null, ex);
+            }
+        }
+  } 
+	
 	
 	// YOU MUST IMPLEMENT THIS METHOD
 	public boolean isStopword( char[] word ) {
+		
 		// return true if the input word is a stopword, or false if not
 		return false;
 	}
--- src/edu/pitt/sis/infsci2140/index/TrecwebCollection.java
+++ src/edu/pitt/sis/infsci2140/index/TrecwebCollection.java
@@ -2,97 +2,84 @@ package edu.pitt.sis.infsci2140.index;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.Map.Entry;
+import java.util.Iterator;
 
 /**
  * This is for INFSCI 2140 in 2015
  *
  */
 public class TrecwebCollection implements DocumentCollection {
+	private Iterator<Entry<String, Object>> iter;
 	
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public TrecwebCollection( FileInputStream instream ) throws IOException {
 		// This constructor should take an inputstream of the collection file as the input parameter.
-		  FileInputStream fis = null;
-	        BufferedReader reader = null;
-	        Map<String,Object> map= new HashMap<String,Object>();
-	        try {
-	            fis = new FileInputStream("D://pitt//data retrieval//docset//docset.trectweb");
-	            reader = new BufferedReader(new InputStreamReader(fis));    
-	            String line = reader.readLine(); 
-	            line=line.replaceAll("<script[^>]*?>[£Ü£Üs£Ü£ÜS]*?<£Ü£Ü/script>", "");//clear script
+		Map<String,Object> map= new HashMap<String,Object>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(instream));    
+         
+        
+        String line;    //read in line
+        String tempkey; //get the line <doc>
+        String key = ""; //get key
+        String text ="";//get text
+        while ((line= reader.readLine()) != null){
+        	int flag=0;
+            while (!line.equals("<DOC>")){
+            	if((line= reader.readLine()) == null)
+            	{
+            		flag=1;
+            		break;
+            	}
+            	line=reader.readLine();
+            }
+            if(flag==1)
+            {
+            	break;
+            }
+            while (line.equals("<DOC>"))
+            {	   
+            	line=reader.readLine();
+            	line=line.replaceAll("<script[^>]*?>[£Ü£Üs£Ü£ÜS]*?<£Ü£Ü/script>", "");//clear script
 	            line=line.replaceAll("<[£Ü£Üs]*?style[^>]*?>[£Ü£Üs£Ü£ÜS]*?<[£Ü£Üs]*?£Ü£Ü/[£Ü£Üs]*?style[£Ü£Üs]*?>", "");//clear style
 	            line=line.replaceAll("</?(?!/?p)[^>]*>", "");//clear all exept <p>
-	            String tempkey;
-	            String key = null;
-	            String text = null;
+            	tempkey=line;
+	            key= tempkey.substring(8,22);//docno
+	            //break;   
+            } 
+            while (!line.equals("<P>")){
+            	line=reader.readLine();
+            	System.out.println(line);
+            }
+            line=reader.readLine();
+            while (!line.equals("</P>")){
+            	text+=line; 
+            	line=reader.readLine();
+            }
+            map.put(key,text.toCharArray());
+            text="";
+        }
+        iter = (Iterator) map.entrySet().iterator(); //iterate map
+}
 	            
-	            
-	            while (line!= null){
-	            	
-	            line=reader.readLine();
-	            
-	                while (line.equals("<DOC>")){	                	
-	            	line=reader.readLine();
-	            	tempkey=line;
-	                key= tempkey.substring(8,15);                   
-	            break;   
-	            }
-	                
-	            while (line!="<p>"){
-	            	line=reader.readLine();
-	            }
-	            
-	            while (line!="</p>"){
-	                	line=reader.readLine();
-	                	text=line;
-	                	text+=text;
-	            }
-	                     map.put(key,text);
-	                     line=reader.readLine();
-	                     line=reader.readLine();
-	            }
-	            Iterator iter = (Iterator) map.entrySet().iterator(); 
-	           // Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
-	    	
-	    		
-	            
-	        }
-
-	                
-	          
-	          
-	        catch (FileNotFoundException ex) {
-	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	        } catch (IOException ex) {
-	            Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	          
-	        } 
-	        finally {
-	            try {
-	                reader.close();
-	                fis.close();
-	            } catch (IOException ex) {
-	                Logger.getLogger(TrectextCollection.class.getName()).log(Level.SEVERE, null, ex);
-	            }
 	        
-	        }
-	}
+
 	
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public Map<String, Object> nextDocument() throws IOException {
 		// Read the definition of this method from edu.pitt.sis.infsci2140.index.DocumentCollection interface 
 		// and follow the assignment instructions to implement this method.
+		Map<String,Object> map2 = new HashMap<String,Object>(); 
+		if(iter.hasNext()) { 
+			Entry<String, Object> entry = (Entry<String, Object>) iter.next();
+			map2.put("DOCNO", entry.getKey());
+			map2.put("CONTENT", entry.getValue());
+			return map2;
+		}
 		return null;
 	}
 	
